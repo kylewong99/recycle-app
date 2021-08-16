3e3560e2b842dcf4ff23c8fb0d2341c428eea610
Kyle <wkl.kylewong@gmail.com>
2021-08-16 22:06:04 +0800

    Integrated forgot password function in forgot password UI

--- app/src/main/java/com/example/recycleapplication/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/recycleapplication/ForgotPasswordActivity.java
@@ -1,22 +1,79 @@
 package com.example.recycleapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
     private Button loginButton;
+    private EditText userEmail;
+    private Button resetPassword;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
-        loginButton = findViewById(R.id.login_btn);
+        loginButton = findViewById(R.id.back_to_login_btn);
+        userEmail = findViewById(R.id.et_email);
+        resetPassword = findViewById(R.id.send_btn);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        resetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = userEmail.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    userEmail.setError("Email cannot empty");
+                    userEmail.requestFocus();
+                } else {
+                    mAuth.sendPasswordResetEmail(userEmail.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.d("Reset Password", "Reset Email Sent");
+                                        Toast.makeText(ForgotPasswordActivity.this, "Successfully send reset password email",
+                                                Toast.LENGTH_SHORT).show();
+                                    } else {
+
+                                        String errorCode = ((FirebaseAuthException)task.getException()).getErrorCode().toString();
+                                        Log.d("Failed to resetPassword", errorCode);
+                                        switch (errorCode) {
+                                            case "ERROR_INVALID_EMAIL":
+                                                userEmail.setError("The email address is badly formatted");
+                                                userEmail.requestFocus();
+                                                break;
+
+                                            case "ERROR_USER_NOT_FOUND":
+                                                userEmail.setError("There is no user record corresponding to this email");
+                                                userEmail.requestFocus();
+                                                break;
+                                        }
+                                    }
+                                }
+                            });
+                }
+            }
+        });
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/recycleapplication/MapViewTest.java
@@ -0,0 +1,42 @@
+package com.example.recycleapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapViewTest extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_view_test);
+
+        // Get a handle to the fragment and register the callback.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    // Get a handle to the GoogleMap object and display marker.
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
