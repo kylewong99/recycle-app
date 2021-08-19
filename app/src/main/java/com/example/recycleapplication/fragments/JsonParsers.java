d90a82624f66ff6f61b49639b2ed0b97f3b99b38
Kyle <wkl.kylewong@gmail.com>
2021-08-19 20:26:22 +0800

    Added nearby search to identify nearby recycling center at Recycling Page

---
+++ app/src/main/java/com/example/recycleapplication/fragments/JsonParsers.java
@@ -0,0 +1,61 @@
+package com.example.recycleapplication.fragments;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class JsonParsers {
+    private HashMap<String,String> parseJsonObject(JSONObject object) {
+        //Initialize hash map
+        HashMap<String,String> dataList = new HashMap();
+        try {
+            //Get name from object
+            String name = object.getString("name");
+            //Get latitude from object
+            String latitude = object.getJSONObject("geometry").getJSONObject("location").getString("lat");
+            //Get longitude from object
+            String longitude = object.getJSONObject("geometry").getJSONObject("location").getString("lng");
+            dataList.put("name",name);
+            dataList.put("lat",latitude);
+            dataList.put("lng",longitude);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        //Return hash map
+        return dataList;
+    }
+
+    private List<HashMap<String,String>> parseJsonArray(JSONArray jsonArray) {
+        //Initialize hash map list
+        List<HashMap<String,String>> dataList = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                //Initialize hash map
+                HashMap<String,String> data = parseJsonObject((JSONObject) jsonArray.get(i));
+                //Add data in hash map list
+                dataList.add(data);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        //Return hash map list
+        return dataList;
+    }
+
+    public List<HashMap<String,String>> parseResult(JSONObject object) {
+        //Initialize json array
+        JSONArray jsonArray = null;
+        //Get result array
+        try {
+            jsonArray = object.getJSONArray("results");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        //Return array
+        return parseJsonArray(jsonArray);
+    }
+}
--- app/src/main/java/com/example/recycleapplication/fragments/RecyclingFragment.java
+++ app/src/main/java/com/example/recycleapplication/fragments/RecyclingFragment.java
@@ -3,16 +3,27 @@ package com.example.recycleapplication.fragments;
 import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,24 +35,51 @@ import android.widget.Toast;
 
 import com.example.recycleapplication.R;
 import com.example.recycleapplication.activity.HomeActivity;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.AutocompletePrediction;
 import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.Autocomplete;
 import com.google.android.libraries.places.widget.AutocompleteActivity;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+import com.google.gson.JsonParser;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,8 +98,16 @@ public class RecyclingFragment extends Fragment implements OnMapReadyCallback {
     private String mParam2;
 
     private GoogleMap mMap;
-
+    private SupportMapFragment mapFragment;
     private EditText searchBar;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private boolean locationPermissionGranted;
+    double currentLat = -33.8523341;
+    double currentLong = 191.2106085;
+
+    private Button test;
+
+    private PlacesClient placesClient;
 
     public RecyclingFragment() {
         // Required empty public constructor
@@ -92,7 +138,6 @@ public class RecyclingFragment extends Fragment implements OnMapReadyCallback {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
     }
 
     @Override
@@ -113,9 +158,12 @@ public class RecyclingFragment extends Fragment implements OnMapReadyCallback {
         Places.initialize(getActivity().getApplicationContext(), "AIzaSyAKiMzIwhYfd0ESn-jIq0iy97Ky9A3GRog");
 
         // Create a new PlacesClient instance
-        PlacesClient placesClient = Places.createClient(getActivity());
+        placesClient = Places.createClient(getActivity());
 
-        SupportMapFragment mapFragment = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map));
+        // Initialize fused location provider client
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        mapFragment = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map));
         if (mapFragment == null) {
             FragmentManager fm = getParentFragmentManager();
             FragmentTransaction ft = fm.beginTransaction();
@@ -130,11 +178,16 @@ public class RecyclingFragment extends Fragment implements OnMapReadyCallback {
         searchBar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                searchBar.setEnabled(false);
                 // Initialize place field list
                 List<Place.Field> fieldList = Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.NAME);
 
                 // Create intent
-                Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY,fieldList).build(getActivity());
+                Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fieldList)
+                        .setCountries(Arrays.asList("my"))
+                        .build(getActivity());
+
 
                 // Start activity result
                 startActivityForResult(intent, 100);
@@ -150,13 +203,18 @@ public class RecyclingFragment extends Fragment implements OnMapReadyCallback {
             if (resultCode == RESULT_OK) {
                 Place place = Autocomplete.getPlaceFromIntent(data);
                 searchBar.setText(place.getAddress());
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 10));
+                currentLat = place.getLatLng().latitude;
+                currentLong = place.getLatLng().longitude;
+                nearbySearch();
             } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
                 // TODO: Handle the error.
                 Status status = Autocomplete.getStatusFromIntent(data);
-                Toast.makeText(getActivity().getApplicationContext(), status.getStatusMessage(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity().getApplicationContext(), status.getStatusMessage(), Toast.LENGTH_SHORT).show();
             } else if (resultCode == RESULT_CANCELED) {
                 // The user canceled the operation.
             }
+            searchBar.setEnabled(true);
             return;
         }
     }
@@ -164,12 +222,146 @@ public class RecyclingFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
+        // Check permission
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // When permission granted
+            // Call method
+            mMap.setMyLocationEnabled(true);
+            mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+                @Override
+                public boolean onMyLocationButtonClick() {
+                    Location myLocation = mMap.getMyLocation();
+                    currentLat = myLocation.getLatitude();
+                    currentLong = myLocation.getLongitude();
+                    nearbySearch();
+                    return false;
+                }
+            });
+            Log.d("location", "location");
+        } else {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    public void nearbySearch() {
+        Log.d("latitude", Double.toString(currentLat));
+        Log.d("longitude", Double.toString(currentLong));
+        String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json" + //url
+                "?location=" + currentLat + "," + currentLong + //Location latitude and Longitude
+                "&radius=5000" + //Nearby radius
+                "&keyword=Recycle center" +
+                "&key=" + "AIzaSyAKiMzIwhYfd0ESn-jIq0iy97Ky9A3GRog";
+
+        //Execute place task method to download json data
+        new PlaceTask().execute(url);
+    }
+
+    private class PlaceTask extends AsyncTask<String, Integer, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String data = null;
+            try {
+                //Initialize data
+                data = downloadUrl(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            //Execute parser task
+            Log.d("url", s);
+            new ParserTask().execute(s);
+        }
+    }
+
+    private String downloadUrl(String string) throws IOException {
+        //Initialize url
+        URL url = new URL(string);
+        //Initialize connection
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        //Connect connection
+        connection.connect();
+        //Initialize input stream
+        InputStream stream = connection.getInputStream();
+        //Initialize buffer reader
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        //Initialize string builder
+        StringBuilder builder = new StringBuilder();
+        //Initialize string variable
+        String line = "";
+        //Use while loop
+        while ((line = reader.readLine()) != null) {
+            //Append line
+            builder.append(line);
+        }
+        //Get append data
+        String data = builder.toString();
+        //Close reader
+        reader.close();
+        //Return data
+        return data;
+    }
+
+    private class ParserTask extends AsyncTask<String, Integer, List<HashMap<String, String>>> {
+        @Override
+        protected List<HashMap<String, String>> doInBackground(String... strings) {
+            //Create json parser class
+            JsonParsers jsonParser = new JsonParsers();
+            //Initialize hash map list
+            List<HashMap<String, String>> mapList = null;
+            JSONObject object = null;
+            try {
+                //Initialize json object
+                object = new JSONObject(strings[0]);
+                //Parse json object
+                mapList = jsonParser.parseResult(object);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            //Return map list
+            return mapList;
+        }
+
+        @Override
+        protected void onPostExecute(List<HashMap<String, String>> hashMaps) {
+            //Clear map
+            mMap.clear();
+            //Use for loop
+            for (int i = 0; i < hashMaps.size(); i++) {
+                //Initialize hash map
+                HashMap<String, String> hashMapList = hashMaps.get(i);
+                //Get latitude
+                double lat = Double.parseDouble(hashMapList.get("lat"));
+                //Get longitude
+                double lng = Double.parseDouble(hashMapList.get("lng"));
+                //Get name
+                String name = hashMapList.get("name");
+                Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+                String placeAddress = "";
+                try {
+                    List<Address> address = geocoder.getFromLocation(lat,lng,1);
+                    placeAddress = address.get(0).getAddressLine(0);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //Concat latitude and longitude
+                LatLng latlng = new LatLng(lat, lng);
+                //Initialize marker options
+                MarkerOptions options = new MarkerOptions();
+                //Set position
+                options.position(latlng);
+                //Set title
+                options.title(name);
+                //Set address
+                options.snippet(placeAddress);
+                //Add marker on map
+                mMap.addMarker(options);
+            }
+        }
+
     }
 }
